93a68a490c8397f22e790803401c58d136648f14
Matthew Wyczalkowski <mwyczalk@genome.wustl.edu>
2018-01-22 22:32:22 -0600

    Staging both MAF and VCF data
    * Scripting runs successfully
    * Stage both MAF and VCF data with same script (stage_data), with flags indicating differnces
    * VCF files are compressed
    * Flags helpful for debugging added to stage_data.sh

--- 1_stage_data.sh
+++ 1_stage_data.sh
@@ -1,11 +1,14 @@
-# Go through all cases and make links to WXS-Germline data
+# Stage WGS-Germline and WGS-Somatic
+# WGS-Germline has large VCF-format output. Compress it (gz) during staging
 
-# Where Song deposited result data, filename e.g., C3L-00010.maf
 ANALYSIS="WGS-Germline"
 DATD="/gscmnt/gc2521/dinglab/scao/cptac3/wgs/germline_per_sample"
-bash ./stage_VCF.sh $ANALYSIS $DATD
+FT="vcf"
+# -z flag to compress large VCFs
+bash ./stage_data.sh -z $ANALYSIS $DATD $FT
 
-
+# Somatic seems is MAF
 ANALYSIS="WGS-Somatic"
 DATD="/gscuser/scao/gc2521/dinglab/scao/cptac3/wgs/somatic_per_sample"
-bash ./stage_VCF.sh $ANALYSIS $DATD
+FT="maf"
+bash ./stage_data.sh $ANALYSIS $DATD $FT
