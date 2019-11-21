de91f222e32b7d603cda1bbc2632591484dfa89d
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2019-11-21 15:34:13 -0600

    Update to not overwrite DCC_analysis_file when multiple summeries of same pipeline

--- 3_update_DCC_summary.sh
+++ 3_update_DCC_summary.sh
@@ -1,5 +1,8 @@
 # Create DCC Analysis Summary file without modifying data in STAGE_ROOT
 
+# NOte, this is done only for old submissions where analysis summary file was not created during staging
+# 
+
 mkdir -p ./dat
 
 bash 1_prep_submission.sh -MNC $@
