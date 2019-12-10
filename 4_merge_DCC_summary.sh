6861eb2689e0e7bffa13a523afa0b1655ec9ab2e
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2019-12-09 22:18:54 -0600

    Improved merging of DCC_Analysis_Summary

--- 4_merge_DCC_summary.sh
+++ 4_merge_DCC_summary.sh
@@ -1,20 +1,43 @@
-DCC_MASTER="/diskmnt/Projects/cptac_scratch/CPTAC3.workflow/CPTAC3.catalog/DCC_Analysis_Summary/RNA-Seq_Fusion.DCC_analysis_summary.dat"
-# If no errors, write merged DCC analysis summary, which is DCC analysis summary and DCC_MASTER concatenated and sorted
-# The output filename will be $BAMMAP.merged
-# If master BAMMAP not defined, exit with no error; if defined but not exist, exit with error
-if [ ! -e $DCC_MASTER ]; then
-    >&2 echo Error: DCC master defined but does not exist.  Exiting
-    >&2 echo DCC_MASTER: $DCC_MASTER
-    exit 1
-fi
+CATALOGD="/diskmnt/Projects/cptac_scratch/CPTAC3.workflow/CPTAC3.catalog/DCC_Analysis_Summary"
+ANALYSES_DAT="analyses.dat"
 
-DCC="dat/RNA-Seq_Fusion.DCC_analysis_summary.dat"
-DCC_MERGED="${DCC}.merged"
 
-head -n1 $DCC > $DCC_MERGED
-cat $DCC_MASTER $DCC | grep -v "^#" | sort -u >> $DCC_MERGED
+function merge_analysis {
+    ANALYSIS=$1
 
->&2 echo $DCC merged with master $DCC_MASTER
->&2 echo and written to merged master $DCC_MERGED
->&2 echo Please examine merged master file and replace original master as appropriate
+    DCC_MASTER="$CATALOGD/${ANALYSIS}.DCC_analysis_summary.dat"
+
+    # If no errors, write merged DCC analysis summary, which is DCC analysis summary and DCC_MASTER concatenated and sorted
+    # The output filename will be $BAMMAP.merged
+    # If master BAMMAP not defined, exit with no error; if defined but not exist, exit with error
+    if [ ! -e $DCC_MASTER ]; then
+        >&2 echo Error: DCC master defined but does not exist.  Exiting
+        >&2 echo DCC_MASTER: $DCC_MASTER
+        exit 1
+    fi
+
+    # DCC as written
+    DCC="dat/${ANALYSIS}.DCC_analysis_summary.dat"
+    DCC_MERGED="dat/${ANALYSIS}.MERGED.DCC_analysis_summary.dat"
+
+    head -n1 $DCC > $DCC_MERGED
+    cat $DCC_MASTER $DCC | grep -v "^#" | sort -u >> $DCC_MERGED
+
+    >&2 echo Batch DCC_Analysis_Summary: $DCC 
+    >&2 echo Master DCC_Analysis_Summary: $DCC_MASTER
+    >&2 echo Merged DCC_Analysis_Summary: $DCC_MERGED
+    >&2 echo Examine and copy Master to Merged 
+    >&2 printf "\n"
+}
+
+
+# Iterate over all entries in analyses.dat
+while read i; do
+    ANALYSIS=$( echo "$i" | cut -f 1 )
+
+    >&2 echo Processing $ANALYSIS
+    merge_analysis $ANALYSIS
+
+
+done < <(sed 's/#.*$//' $ANALYSES_DAT | sed '/^\s*$/d' )  # skip comments and blank lines. May require `set +o posix`
 
