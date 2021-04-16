94359ed47c9c3311275046b306aabf200e417eb8
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2021-04-15 20:12:38 -0500

    Adding notes about -x 5 flag for Transcript

--- 1_prep_submission.sh
+++ 1_prep_submission.sh
@@ -1,4 +1,7 @@
 mkdir -p dat
 
+# prepend tissue type to case name for RNA-Seq_Transcript
+# ARG="-x 5"
+
 # prep_submission.sh reads analyses.dat, batch.dat, system.dat
-src/prep_submission.sh -C $@
+src/prep_submission.sh $ARG -C $@
