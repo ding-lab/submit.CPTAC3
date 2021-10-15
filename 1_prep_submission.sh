e68c5b8100ab34809a849b91da51e3f125328558
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2021-10-15 16:27:10 -0500

    Workflow documentation updates

--- 1_prep_submission.sh
+++ 1_prep_submission.sh
@@ -1,7 +1,19 @@
 mkdir -p dat
 
 # prepend tissue type to case name for RNA-Seq_Transcript
-# ARG="-x 5"
+ARG="-x 5"
 
 # prep_submission.sh reads analyses.dat, batch.dat, system.dat
 src/prep_submission.sh $ARG -C $@
+
+>&2 echo NOTE: any DCC analysis summary files written now 
+>&2 echo will be appended to if this script is run again
+>&2 echo To avoid corruption, delete any existing DCC analysis
+>&2 echo files
+
+# The reason this is not easy to fix is because the analysis.dat file can have any number of 
+# pipelines, and if 2+ batches of the same pipeline are processed then it is expected that
+# the results will accumulate.  In this scenario, it is expected that an existing analysis
+# summary file is appended to.  This can lead to corruption, though, if the same data is
+# staged multiple times; each invocation will append to the DCC analysis summary file
+# In most cases, easiest is to just remove duplicate lines
--- 3x.update_DCC_summary.sh
+++ 3x.update_DCC_summary.sh
@@ -1,6 +1,7 @@
 # Create DCC Analysis Summary file without modifying data in STAGE_ROOT
 
-# NOte, this is done only for old submissions where analysis summary file was not created during staging
+# NOte, this is done only for old submissions where DCC analysis summary file was not created during staging,
+# or when the DCC analysis summary file was corrupted or lost
 # 
 
 mkdir -p ./dat
