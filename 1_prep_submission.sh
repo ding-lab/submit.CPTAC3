43aff796e89057059ffb00d94a2d72d8be44e04b
Matthew Wyczalkowski <mwyczalk@genome.wustl.edu>
2021-10-27 17:48:12 -0500

    Removing -x 5 from default arguments when staging

--- 1_prep_submission.sh
+++ 1_prep_submission.sh
@@ -1,7 +1,7 @@
 mkdir -p dat
 
 # prepend tissue type to case name for RNA-Seq_Transcript
-ARG="-x 5"
+#ARG="-x 5"
 
 # prep_submission.sh reads analyses.dat, batch.dat, system.dat
 src/prep_submission.sh $ARG -C $@
